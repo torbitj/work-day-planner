4cbf6fa2f72bc621c17bda5c038d85c31c59b3d3
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-08-25 14:27:18 -0600

    created if statement within the for each loop to have row backgrounds match the time

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,11 +1,13 @@
 var timeContainer = $('#row-container');
 var currentDay = $('#currentDay');
 var todayDate = moment().format('MMM Do YYYY');
+var currentTime= moment().format('H')
+console.log(currentTime)
 console.log(timeContainer);
 // time objects with content
 var timeCards = [
     {
-        time: '9AM',
+        time: "9AM",
         tasks: '',
         save: ''
     },
@@ -52,17 +54,26 @@ var timeCards = [
 ]
 
 timeCards.forEach(function(timeCards){
-
+    var timeConvert = moment(timeCards.time, 'hA').format('H');
     var rowEl = $('<tr>').addClass('row time-block col-12');
-    var timeHead = $('<th>').addClass('hour col').text(timeCards.time);
+    var timeHead = $('<th>').addClass('hour col').attr('data-mt', 'd'.replace(/d/g, timeConvert)).text(timeCards.time);
     var taskEl = $('<td>').addClass('col-10').text(timeCards.tasks);
     var saveEl = $('<td>').addClass('col btn saveBtn').text(timeCards.save);
     var saveIcon = $('<i>').addClass('fas fa-save fa-lg');
+    
+
+    if (timeHead.data('mt') == currentTime) {
+        taskEl.addClass(' present')
+    } else if (timeHead.data('mt') < currentTime) {
+        taskEl.addClass(' past')
+    } else {
+        taskEl.addClass(' future')
+    }
 
     saveEl.append(saveIcon);
     rowEl.append(timeHead, taskEl, saveEl);
     timeContainer.append(rowEl);
-    
+    console.log(timeConvert)
 })
 
 currentDay.text(todayDate)
